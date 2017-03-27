577846a676f554b97374a1354bb0ceca67659b33
Rick Liu <rickliu2000@outlook.com>
2017-03-27 09:56:16 +0800

    partly dealt with html tags

--- app/src/main/java/io/nfls/forum/DiscussionDetail.java
+++ app/src/main/java/io/nfls/forum/DiscussionDetail.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -190,7 +191,8 @@ public class DiscussionDetail extends AppCompatActivity{
                                     list.add("Discussion Tagged By" + participants[j]);
                                     System.out.println("Discussion Tagged By" + participants[j]);
                                 } else{
-                                    list.add(participants[j] + ":" + comment[i]);
+                                    String html=participants[j] + ":" + comment[i];
+                                    list.add(String.valueOf(Html.fromHtml(html, null, new SizeLabel(20))));
                                     System.out.println(participants[j] + ":" + comment[i]);
                                 }
                             }
